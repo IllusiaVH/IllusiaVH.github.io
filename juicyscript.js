745acaf1120a119a5203eb58dddf7582aa4ad51a
IllusiaVH <97069070+IllusiaVH@users.noreply.github.com>
2022-01-14 08:51:01 +0800

    Add files via upload

---
+++ juicyscript.js
@@ -0,0 +1,14 @@
+// hello juicyscript
+console.log("");
+
+document.getElementById("btn1").onclick = function(){
+  document.getElementById("frontintro").style.color = "red";
+}
+
+document.getElementById("btn2").onclick = function(){
+  document.getElementById("otherdetails").style.color = "green";
+}
+
+document.getElementById("btn3").onclick = function(){
+  document.getElementById("boxmeme").style.color = "blue";
+}
